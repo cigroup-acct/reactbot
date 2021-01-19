c655ba5286bce70a4e0ef53a0c65d0b09e17664c
John Odey <odeyjohn4u@gmail.com>
2021-01-19 10:55:50 +0100

    added dialogflow routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send({
-        'name': 'John Odey'
-    });
-});
+// Add dialogflow routes and make app available in the file
+require('./routes/dialogFlowRoutes')(app);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,20 @@
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send({
+            'name': 'John Odey'
+        });
+    });
+
+    app.post('/api/df_text_query', (req, res) => {
+        res.send({
+            'do': 'text query'
+        });
+    });
+
+    app.post('/api/df_event_query', (req, res) => {
+        res.send({
+            'do': 'event query'
+        });
+    });
+    
+}
