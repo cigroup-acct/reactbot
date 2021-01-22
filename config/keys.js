8d3565ade3a8e424a0ad08809158dffc27abd01d
John Odey <odeyjohn4u@gmail.com>
2021-01-22 16:48:47 +0100

    fixed config file

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV == "development") {
-    module.exports = require("./dev");
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./prod");
 }
 else {
-    module.exports = require("./prod");
+    module.exports = require("./dev");
 }
